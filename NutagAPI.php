69947dd8a07c1d2195475354256e4014c435e06f
Maradona Morais <maradona.morais@hotmail.com>
2016-04-24 15:32:39 -0300

    explode no sobrenome

--- NutagAPI.php
+++ NutagAPI.php
@@ -6,7 +6,7 @@
 		public function authUser($matricula, $senha) {
 			$data = array("matricula"=> $matricula, "senha"=>$senha);
 
-			$url = "http://api.nutag.info/auth";
+			$url = "https://nutag.websiteseguro.com/api/auth/";
 			$options = array(
 			    'http' => array(
 			        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -21,9 +21,9 @@
 
 			$result = json_decode($result);
 			if (isset($result->type) && $result->type=="error") {
-				return $result;
+				return false;
 			} else {
-				$user = new IFRN_User($matricula, $result->nome, $result->email);
+				$user = new IFRN_User($matricula, $result->nome, $result->sobrenome, $result->email);
 				return array("type"=>"success", "msg"=>"user_ok", "user"=>$user);
 			}
 
--- lib/IFRN_User.php
+++ lib/IFRN_User.php
@@ -3,12 +3,14 @@
 class IFRN_User {
 	public $matricula;
 	public $nome;
+	public $sobrenome;
 	public $email;
 	
-	function __construct($matricula, $nome, $email)
+	function __construct($matricula, $nome, $sobrenome, $email)
 	{
 		$this->matricula = $matricula;
 		$this->nome = $nome;
+		$this->sobrenome = $sobrenome;
 		$this->email = $email;
 	}
 }
